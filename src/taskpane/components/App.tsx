613c05131dd1a3910f75c0c76b309896d015cd5e
Jerónimo Sancho <jsanchoh@capgemini.com>
2019-07-25 08:29:31 +0200

    fix excel reference error

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -9,13 +9,16 @@ import { EmployeeData } from './shared/model/interfaces/EmployeeData';
 import { ProjectData } from './shared/model/interfaces/ProjectData';
 import { ProjectsPanel } from './ProjectsPanelComponent';
 
-export default class App extends React.Component<{}, {
-  projectsSheet: Excel.Worksheet,
-  projects: Excel.Range,
-  hoursList: HoursList[],
-  dataLoaded: boolean,
-  employeeName: string,
-}> {
+export default class App extends React.Component<
+  {},
+  {
+    projectsSheet: Excel.Worksheet;
+    projects: Excel.Range;
+    hoursList: HoursList[];
+    dataLoaded: boolean;
+    employeeName: string;
+  }
+> {
   constructor(props: any, context: Excel.RequestContext) {
     super(props, context);
     handleHourChange.bind(this);
@@ -35,26 +38,28 @@ export default class App extends React.Component<{}, {
 
   // Called every time the user click on a cell
   clickListener = async () => {
-      await Excel.run(async (context) => {
-        context.workbook.worksheets.getFirst().onSelectionChanged.add(this.click); // Check if the selected cell has changed
-        await context.sync()
-      });
-  }
+    await Excel.run(async (context) => {
+      console.log('Employee listener wroking');
+      context.workbook.worksheets.getFirst().onSelectionChanged.add(this.click); // Check if the selected cell has changed
+      await context.sync();
+    });
+  };
 
   // Get projects' data of the selected Employee
   click = async () => {
     try {
-      return Excel.run(async context => {
+      return Excel.run(async (context) => {
+        console.log('Employee clicked');
         this.setState({
           projectsSheet: null,
           projects: null,
           hoursList: [],
-          dataLoaded: false
+          dataLoaded: false,
         }); // Reset state to empty / false
 
         const employeeData: EmployeeData = {
           category: null,
-          activeEmployee: null
+          activeEmployee: null,
         };
         await getSelectedEmployeeData(context).then((res: any) => {
           employeeData.category = res.selectedCat;
@@ -62,22 +67,28 @@ export default class App extends React.Component<{}, {
         });
 
         this.setState({
-          projectsSheet: context.workbook.worksheets.getItem(employeeData.category.values[0][0]).load("name") // Get the Excel sheet containing projects and their data 
+          projectsSheet: context.workbook.worksheets
+            .getItem(employeeData.category.values[0][0])
+            .load('name'), // Get the Excel sheet containing projects and their data
         });
 
         const projData: ProjectData = {
           firstCell: null,
           lastCell: null,
-          employeeCol: null
-        }
-        await getProjectsData(context, employeeData, this.state).then((res: any) => {
-          projData.firstCell = res.first;
-          projData.lastCell = res.last;
-          projData.employeeCol = res.employeeCol;
-        });
+          employeeCol: null,
+        };
+        await getProjectsData(context, employeeData, this.state).then(
+          (res: any) => {
+            projData.firstCell = res.first;
+            projData.lastCell = res.last;
+            projData.employeeCol = res.employeeCol;
+          },
+        );
 
         this.setState({
-          projects: this.state.projectsSheet.getRange(projData.firstCell + ":" + projData.lastCell).load("values") // Set the state projects with the projects from the sheet with their data
+          projects: this.state.projectsSheet
+            .getRange(projData.firstCell + ':' + projData.lastCell)
+            .load('values'), // Set the state projects with the projects from the sheet with their data
         });
         await context.sync();
 
@@ -85,24 +96,27 @@ export default class App extends React.Component<{}, {
 
         this.setState({
           employeeName: employeeData.activeEmployee.values[0][0], // Set the state name with the selected Employee
-          dataLoaded: true // Set the state dataLoaded to true once the data is ready to be displayed
+          dataLoaded: true, // Set the state dataLoaded to true once the data is ready to be displayed
         });
-
       });
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   render() {
     return (
-      <div className='ms-welcome'>
-        {this.state.dataLoaded &&
-        <div>
-          <AddProject state={this.state} projSheet={this.state.projectsSheet} click={this.click} ></AddProject>
-          <ProjectsPanel state={this.state} ></ProjectsPanel>
+      <div className="ms-welcome">
+        {this.state.dataLoaded && (
+          <div>
+            <AddProject
+              state={this.state}
+              projSheet={this.state.projectsSheet}
+              click={this.click}
+            />
+            <ProjectsPanel state={this.state} />
           </div>
-        }
+        )}
       </div>
     );
   }
