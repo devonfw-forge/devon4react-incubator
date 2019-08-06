dfb7475f38a1d7b6203e7925c2899dfc090a3e94
Jerónimo Sancho <jsanchoh@capgemini.com>
2019-08-06 14:46:46 +0200

    projectpanelcomponent renamed to projectpanel

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ErrorHandling } from './ErrorHandling';
-import { ProjectsPanel } from './ProjectsPanelComponent';
+import { ProjectsPanel } from './ProjectsPanel';
 import {
   CALC,
   ERRORS,
