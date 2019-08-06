7d4765f493906aaa286b96bf4302f40cdd8b9d04
Jerónimo Sancho <jsanchoh@capgemini.com>
2019-08-06 08:33:30 +0200

    wpi

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { ErrorHandling } from './ErrorHandling';
 import { ProjectsPanel } from './ProjectsPanelComponent';
 // import { handleOnChange } from './SaveHour';
-import { getSelectedEmployeeData } from './SelectedEmployee';
 import {
   CALC,
   ERRORS,
